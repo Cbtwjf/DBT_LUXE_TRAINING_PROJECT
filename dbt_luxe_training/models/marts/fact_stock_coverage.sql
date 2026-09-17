a6bc512bb3da6cd5af099391cc817c2fb2a3752b
Cbtwjf <jferquel@cbtw.tech>
2026-09-17 12:08:30 +0200

    Add files via upload

---
+++ dbt_luxe_training/models/marts/fact_stock_coverage.sql
@@ -0,0 +1,18 @@
+SELECT
+    i.product_id,
+    i.store_id,
+    i.stock_available,
+    s.qty_sold_30d,
+    s.avg_daily_sales,
+
+    CASE
+        WHEN s.avg_daily_sales > 0
+        THEN i.stock_available / s.avg_daily_sales
+        ELSE NULL
+    END AS stock_coverage_days
+
+FROM {{ ref('int_inventory_current') }} i
+
+LEFT JOIN {{ ref('int_sales_30d') }} s
+    ON i.product_id = s.product_id
+    AND i.store_id = s.store_id
