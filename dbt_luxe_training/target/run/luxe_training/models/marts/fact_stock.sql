f2a9bf635d1a25aa4ef80e6c121210d9acc073f4
jferquel@cbtw.tech <jferquel@cbtw.tech>
2026-09-25 15:33:17 +0000

    2509

---
+++ dbt_luxe_training/target/run/luxe_training/models/marts/fact_stock.sql
@@ -0,0 +1,31 @@
+
+  
+    
+
+        create or replace transient table luxe_training.dwh.fact_stock
+         as
+        (SELECT
+    i.product_id,
+    i.store_id,
+    i.stock_qty,
+
+    s1.qty_sold_30d,
+    s1.avg_daily_sales,
+    s1.stock_coverage_days,
+
+    s2.days_observed,
+    s2.stockout_days,
+    s2.stockout_rate
+
+FROM luxe_training.dwh.int_inventory_current i
+
+LEFT JOIN luxe_training.dwh.fact_stock_coverage s1
+    ON i.product_id = s1.product_id
+    AND i.store_id = s1.store_id
+
+LEFT JOIN luxe_training.dwh.fact_stockout_rate s2
+    ON i.product_id = s2.product_id
+    AND i.store_id = s2.store_id
+        );
+      
+  
